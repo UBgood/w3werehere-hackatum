b8d60cc539dd37413ec0cca1e8619b3eef13a16c
UBgood <dj.indiana35@gmail.com>
2024-11-23 18:57:44 +0200

    edited lib.rs and test file. works

--- project/programs/project/src/lib.rs
+++ project/programs/project/src/lib.rs
@@ -2,61 +2,58 @@ use anchor_lang::prelude::*;
 
 declare_id!("ANdYmC2Z4yKnQeLq4GodFPMcqJGaq9y4DEbeJcBU7PHV");
 
+// Instructions defined in program module
 #[program]
 pub mod project {
     use super::*;
-
+ 
+    // Instruction to initialize a new project account
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        // Reference to the project account from the Initialize struct
+        let project = &ctx.accounts.project;
+        msg!("Project account created! Current count: {}", project.count);
         Ok(())
     }
-
-    pub fn login(ctx: Context<Initialize>) -> Result<()> { //scan QR code
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-
-    pub fn logout(ctx: Context<Initialize>) -> Result<()> { //leave venue and earn points/SOL
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-
-    pub fn read(ctx: Context<Initialize>) -> Result<()> { //read and earn point/SOL
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-
-    pub fn write(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+ 
+    // Instruction to increment a project account
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        // Mutable reference to the project account from the Increment struct
+        let project = &mut ctx.accounts.project;
+        msg!("Previous project: {}", project.count);
+ 
+        // Increment the count value stored on the project account by 1
+        project.count = project.count.checked_add(1).unwrap();
+        msg!("Project incremented! Current count: {}", project.count);
         Ok(())
     }
 }
-
+ 
+// Accounts required by the initialize instruction
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    // The account paying to create the wwh account
+    // The account paying to create the project account
     #[account(mut)]
     pub user: Signer<'info>, // specify account must be signer on the transaction
  
-    // The wwh account being created and initialized in the instruction
+    // The project account being created and initialized in the instruction
     #[account(
         init,         // specifies we are creating this account
         payer = user, // specifies account paying for the creation of the account
         space = 8 + 8 // space allocated to the new account (8 byte discriminator + 8 byte for u64)
     )]
-    pub wwh: Account<'info, wwh>, // specify account is 'wwh' type
+    pub project: Account<'info, Project>, // specify account is 'Project' type
     pub system_program: Program<'info, System>, // specify account must be System Program
 }
-
+ 
 // Account required by the increment instruction
 #[derive(Accounts)]
 pub struct Increment<'info> {
     #[account(mut)] // specify account is mutable because we are updating its data
-    pub wwh: Account<'info, wwh>, // specify account is 'wwh' type
+    pub project: Account<'info, Project>, // specify account is 'Project' type
 }
  
-// Define structure of `wwh` account
+// Define structure of `Project` account
 #[account]
-pub struct wwh {
+pub struct Project {
     pub count: u64, // define count value type as u64
 }
--- project/tests/project.ts
+++ project/tests/project.ts
@@ -1,16 +1,52 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Project } from "../target/types/project";
+import { Keypair } from "@solana/web3.js";
 
 describe("project", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+ 
   const program = anchor.workspace.Project as Program<Project>;
-
+ 
+  // Generate a new keypair to use as the address the project account
+  const projectAccount = new Keypair();
+ 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    // Invoke the initialize instruction
+    const transactionSignature = await program.methods
+      .initialize()
+      .accounts({
+        project: projectAccount.publicKey,
+      })
+      .signers([projectAccount]) // include project keypair as additional signer
+      .rpc({ skipPreflight: true });
+ 
+    // Fetch the project account data
+    const accountData = await program.account.project.fetch(
+      projectAccount.publicKey,
+    );
+ 
+    console.log(`Transaction Signature: ${transactionSignature}`);
+    console.log(`Count: ${accountData.count}`);
+  });
+ 
+  it("Increment", async () => {
+    // Invoke the increment instruction
+    const transactionSignature = await program.methods
+      .increment()
+      .accounts({
+        project: projectAccount.publicKey,
+      })
+      .rpc();
+ 
+    // Fetch the project account data
+    const accountData = await program.account.project.fetch(
+      projectAccount.publicKey,
+    );
+ 
+    console.log(`Transaction Signature: ${transactionSignature}`);
+    console.log(`Count: ${accountData.count}`);
   });
 });
