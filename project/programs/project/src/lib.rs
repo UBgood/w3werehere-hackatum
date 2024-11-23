bab74f5cc31a688013d7294ee3b888e3c4e2a697
UBgood <dj.indiana35@gmail.com>
2024-11-23 20:53:09 +0200

    nft minter code and previous changes to anchor.toml and test file

--- project/programs/project/src/lib.rs
+++ project/programs/project/src/lib.rs
@@ -1,59 +1,148 @@
-use anchor_lang::prelude::*;
+#![allow(clippy::result_large_err)]
+
+use {
+    anchor_lang::prelude::*,
+    anchor_spl::{
+        associated_token::AssociatedToken,
+        metadata::{
+            create_master_edition_v3, create_metadata_accounts_v3,
+            mpl_token_metadata::types::DataV2, CreateMasterEditionV3, CreateMetadataAccountsV3,
+            Metadata,
+        },
+        token::{mint_to, Mint, MintTo, Token, TokenAccount},
+    },
+};
 
 declare_id!("ANdYmC2Z4yKnQeLq4GodFPMcqJGaq9y4DEbeJcBU7PHV");
 
-// Instructions defined in program module
 #[program]
-pub mod project {
+pub mod nft_minter {
     use super::*;
- 
-    // Instruction to initialize a new project account
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        // Reference to the project account from the Initialize struct
-        let project = &ctx.accounts.project;
-        msg!("Project account created! Current count: {}", project.count);
-        Ok(())
-    }
- 
-    // Instruction to increment a project account
-    pub fn increment(ctx: Context<Increment>) -> Result<()> {
-        // Mutable reference to the project account from the Increment struct
-        let project = &mut ctx.accounts.project;
-        msg!("Previous project: {}", project.count);
- 
-        // Increment the count value stored on the project account by 1
-        project.count = project.count.checked_add(1).unwrap();
-        msg!("Project incremented! Current count: {}", project.count);
+
+    pub fn mint_nft(
+        ctx: Context<CreateToken>,
+        nft_name: String,
+        nft_symbol: String,
+        nft_uri: String,
+    ) -> Result<()> {
+        msg!("Minting Token");
+        // Cross Program Invocation (CPI)
+        // Invoking the mint_to instruction on the token program
+        mint_to(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    mint: ctx.accounts.mint_account.to_account_info(),
+                    to: ctx.accounts.associated_token_account.to_account_info(),
+                    authority: ctx.accounts.payer.to_account_info(),
+                },
+            ),
+            1,
+        )?;
+
+        msg!("Creating metadata account");
+        // Cross Program Invocation (CPI)
+        // Invoking the create_metadata_account_v3 instruction on the token metadata program
+        create_metadata_accounts_v3(
+            CpiContext::new(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    metadata: ctx.accounts.metadata_account.to_account_info(),
+                    mint: ctx.accounts.mint_account.to_account_info(),
+                    mint_authority: ctx.accounts.payer.to_account_info(),
+                    update_authority: ctx.accounts.payer.to_account_info(),
+                    payer: ctx.accounts.payer.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+            ),
+            DataV2 {
+                name: nft_name,
+                symbol: nft_symbol,
+                uri: nft_uri,
+                seller_fee_basis_points: 0,
+                creators: None,
+                collection: None,
+                uses: None,
+            },
+            false, // Is mutable
+            true,  // Update authority is signer
+            None,  // Collection details
+        )?;
+
+        msg!("Creating master edition account");
+        // Cross Program Invocation (CPI)
+        // Invoking the create_master_edition_v3 instruction on the token metadata program
+        create_master_edition_v3(
+            CpiContext::new(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMasterEditionV3 {
+                    edition: ctx.accounts.edition_account.to_account_info(),
+                    mint: ctx.accounts.mint_account.to_account_info(),
+                    update_authority: ctx.accounts.payer.to_account_info(),
+                    mint_authority: ctx.accounts.payer.to_account_info(),
+                    payer: ctx.accounts.payer.to_account_info(),
+                    metadata: ctx.accounts.metadata_account.to_account_info(),
+                    token_program: ctx.accounts.token_program.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+            ),
+            None, // Max Supply
+        )?;
+
+        msg!("NFT minted successfully.");
+
         Ok(())
     }
 }
- 
-// Accounts required by the initialize instruction
+
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    // The account paying to create the project account
+pub struct CreateToken<'info> {
     #[account(mut)]
-    pub user: Signer<'info>, // specify account must be signer on the transaction
- 
-    // The project account being created and initialized in the instruction
+    pub payer: Signer<'info>,
+
+    /// CHECK: Validate address by deriving pda
     #[account(
-        init,         // specifies we are creating this account
-        payer = user, // specifies account paying for the creation of the account
-        space = 8 + 8 // space allocated to the new account (8 byte discriminator + 8 byte for u64)
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), mint_account.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key(),
     )]
-    pub project: Account<'info, Project>, // specify account is 'Project' type
-    pub system_program: Program<'info, System>, // specify account must be System Program
-}
- 
-// Account required by the increment instruction
-#[derive(Accounts)]
-pub struct Increment<'info> {
-    #[account(mut)] // specify account is mutable because we are updating its data
-    pub project: Account<'info, Project>, // specify account is 'Project' type
-}
- 
-// Define structure of `Project` account
-#[account]
-pub struct Project {
-    pub count: u64, // define count value type as u64
+    pub metadata_account: UncheckedAccount<'info>,
+
+    /// CHECK: Validate address by deriving pda
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), mint_account.key().as_ref(), b"edition"],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    pub edition_account: UncheckedAccount<'info>,
+
+    // Create new mint account, NFTs have 0 decimals
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = 0,
+        mint::authority = payer.key(),
+        mint::freeze_authority = payer.key(),
+    )]
+    pub mint_account: Account<'info, Mint>,
+
+    // Create associated token account, if needed
+    // This is the account that will hold the NFT
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint_account,
+        associated_token::authority = payer,
+    )]
+    pub associated_token_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
 }
--- project/tests/project.ts
+++ project/tests/project.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Project } from "../target/types/project";
-import { Keypair } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 
 describe("project", () => {
   // Configure the client to use the local cluster.
@@ -11,25 +11,48 @@ describe("project", () => {
   const program = anchor.workspace.Project as Program<Project>;
  
   // Generate a new keypair to use as the address the project account
-  const projectAccount = new Keypair();
+  //const projectAccount = new Keypair();
+
+  const [projectPDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from("project")],
+    program.programId,
+  );
  
+  // it("Is initialized!", async () => {
+  //   // Invoke the initialize instruction
+  //   const transactionSignature = await program.methods
+  //     .initialize()
+  //     .accounts({
+  //       project: projectAccount.publicKey,
+  //     })
+  //     .signers([projectAccount]) // include project keypair as additional signer
+  //     .rpc({ skipPreflight: true });
+ 
+  //   // Fetch the project account data
+  //   const accountData = await program.account.project.fetch(
+  //     projectAccount.publicKey,
+  //   );
+ 
+  //   console.log(`Transaction Signature: ${transactionSignature}`);
+  //   console.log(`Count: ${accountData.count}`);
+  // });
+
   it("Is initialized!", async () => {
-    // Invoke the initialize instruction
-    const transactionSignature = await program.methods
-      .initialize()
-      .accounts({
-        project: projectAccount.publicKey,
-      })
-      .signers([projectAccount]) // include project keypair as additional signer
-      .rpc({ skipPreflight: true });
+    try {
+      const txSig = await program.methods
+        .initialize()
+        .accounts({
+          project: projectPDAPDA,
+        })
+        .rpc();
  
-    // Fetch the project account data
-    const accountData = await program.account.project.fetch(
-      projectAccount.publicKey,
-    );
- 
-    console.log(`Transaction Signature: ${transactionSignature}`);
-    console.log(`Count: ${accountData.count}`);
+      const accountData = await program.account.project.fetch(projectPDA);
+      console.log(`Transaction Signature: ${txSig}`);
+      console.log(`Count: ${accountData.count}`);
+    } catch (error) {
+      // If PDA Account already created, then we expect an error
+      console.log(error);
+    }
   });
  
   it("Increment", async () => {
@@ -37,13 +60,13 @@ describe("project", () => {
     const transactionSignature = await program.methods
       .increment()
       .accounts({
-        project: projectAccount.publicKey,
+        project: projectPDA,
       })
       .rpc();
  
     // Fetch the project account data
     const accountData = await program.account.project.fetch(
-      projectAccount.publicKey,
+      projectPDA
     );
  
     console.log(`Transaction Signature: ${transactionSignature}`);
